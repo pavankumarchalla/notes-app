04a83949ece4f31c37d922e154dba083232e633c
Pavan Kumar C <pavankumar@railsfactory.org>
2019-03-04 18:29:10 +0530

    list functionality implemented

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -39,6 +39,16 @@ const removeNote = (title) => {
     
 }
 
+const listNotes = () => {
+    const notes = loadNotes()
+
+    console.log(chalk.inverse('your notes'))
+    notes.forEach(note => {
+        console.log(note.title)
+    });
+
+}
+
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
@@ -58,5 +68,6 @@ const loadNotes = () => {
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes
 }
